5cdbcf098b8555ec488d6e522479acfd049463d8
leoFedora <anlleguizamongu@unal.edu.co>
2024-02-05 21:10:25 -0500

    refav

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="cabecera" class="sticky-top">
+		<nav class="navbar bg-body-tertiary h-50">
+			<div class="container-fluid">
+				<a class="navbar-brand">
+					<img src="./assets/rosa.webp" alt="A" width="30" height="30"
+						class="rounded d-inline-block align-text-top">
+					Powered by: Abakus Accounting
+				</a>
+			</div>
+		</nav>
+	</div>
+	<div id="Contenido">
+		<div class="tab-pane fade show active" id="home" tabindex="0">
+		</div>
+	</div>
+	<div id="footer" class="nav justify-content-center bg-body-tertiary fixed-bottom">
+		<a class="nav-tabContent navbar-brand" href="https://github.com/leoleguizamon97" target="_blank">
+			<img class="rounded" alt="ALLG" src="./assets/rosa.webp" width="15" height="15">
+			anlleguizamongu
+		</a>
+	</div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
