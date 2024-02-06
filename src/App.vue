0635c4454928380ed608c3e0dce2a486291a120e
leoFedora <anlleguizamongu@unal.edu.co>
2024-02-06 01:44:53 -0500

    Menu de articulos y modo color

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-	<div id="cabecera" class="sticky-top">
-		<nav class="navbar bg-body-tertiary h-50">
-			<div class="container-fluid">
-				<a class="navbar-brand">
-					<img src="./assets/rosa.webp" alt="A" width="30" height="30"
-						class="rounded d-inline-block align-text-top">
-					Powered by: Abakus Accounting
-				</a>
+	<CabeceraN />
+	<div class=" h-100 d-flex flex-column overflow-auto" style="margin-bottom:25px;">
+		<div id="Contenido" class="flex-grow-1 d-flex">
+			<div class="d-flex align-items-center ">
+				<MenuP />
 			</div>
-		</nav>
-	</div>
-	<div id="Contenido">
-		<div class="tab-pane fade show active" id="home" tabindex="0">
+			<div class="tab-content flex-grow-1 border rounded-2 p-1 my-1" id="v-pills-tabContent">
+				<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"
+					tabindex="0">
+					Este es el contenido
+				</div>
+				<div class="tab-pane fade" id="v-pills-1001" role="tabpanel" aria-labelledby="v-pills-1001-tab"
+					tabindex="0">
+					1001</div>
+				<div class="tab-pane fade" id="v-pills-1002" role="tabpanel" aria-labelledby="v-pills-1002-tab"
+					tabindex="0">
+					1002</div>
+				<div class="tab-pane fade" id="v-pills-1003" role="tabpanel" aria-labelledby="v-pills-1003-tab"
+					tabindex="0">
+					1003</div>
+				<div class="tab-pane fade" id="v-pills-1004" role="tabpanel" aria-labelledby="v-pills-1004-tab"
+					tabindex="0">
+					1004</div>
+				<div class="tab-pane fade" id="v-pills-1005" role="tabpanel" aria-labelledby="v-pills-1005-tab"
+					tabindex="0">
+					1005</div>
+				<div class="tab-pane fade" id="v-pills-1006" role="tabpanel" aria-labelledby="v-pills-1006-tab"
+					tabindex="0">
+					1006</div>
+				<div class="tab-pane fade" id="v-pills-1007" role="tabpanel" aria-labelledby="v-pills-1007-tab"
+					tabindex="0">
+					1007</div>
+				<div class="tab-pane fade" id="v-pills-1008" role="tabpanel" aria-labelledby="v-pills-1008-tab"
+					tabindex="0">
+					1008</div>
+				<div class="tab-pane fade" id="v-pills-1009" role="tabpanel" aria-labelledby="v-pills-1009-tab"
+					tabindex="0">
+					1009</div>
+				<div class="tab-pane fade" id="v-pills-1010" role="tabpanel" aria-labelledby="v-pills-1010-tab"
+					tabindex="0">
+					1010</div>
+				<div class="tab-pane fade" id="v-pills-1011" role="tabpanel" aria-labelledby="v-pills-1011-tab"
+					tabindex="0">
+					1011</div>
+				<div class="tab-pane fade" id="v-pills-1012" role="tabpanel" aria-labelledby="v-pills-1012-tab"
+					tabindex="0">
+					1012</div>
+
+			</div>
 		</div>
 	</div>
 	<div id="footer" class="nav justify-content-center bg-body-tertiary fixed-bottom">
@@ -22,5 +58,14 @@
 	</div>
 </template>
 <script>
+import MenuP from '@/components/MenuP.vue'
+import CabeceraN from '@/components/CabeceraN.vue'
+export default {
+	name: 'App',
+	components: {
+		MenuP,
+		CabeceraN,
+	},
+}
 </script>
 <style></style>
---
+++ src/components/CabeceraN.vue
@@ -0,0 +1,37 @@
+<template>
+	<nav id="cabecera" class="navbar sticky-top bg-body-tertiary p-1">
+		<div class="container-fluid p-0">
+			<a class="navbar-brand p-0">
+				<img src="@/assets/rosa.webp" alt="A" width="35" height="35" class="rounded-5 p-0">
+				Powered by: Abakus Accounting
+			</a>
+			<div type="btn" class="btn border-0" v-on:click=cambiartema(this.luz)>
+				<i v-if=luz class="bi bi-sun"></i>
+				<i v-else class="bi bi-moon"></i>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {
+	methods: {
+		cambiartema(arg) {
+			this.luz = !arg
+			if (this.luz) {
+				console.log('luz');
+				document.documentElement.setAttribute('data-bs-theme', 'ligth')
+			} else {
+				console.log('oscuro');
+				document.documentElement.setAttribute('data-bs-theme', 'dark')
+			}
+		}
+	},
+	data() {
+		return {
+			luz: false
+		}
+	}
+}
+</script>
+<style></style>
