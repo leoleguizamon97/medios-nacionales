857eff7eef51b85c02e4d894d1417fb806edf556
leoleguizamon <anlleguizamongu@unal.edu.co>
2024-04-06 23:16:34 -0500

    Eliminacion de DBSQL

--- Back/server.js
+++ Back/server.js
@@ -1,10 +1,10 @@
-const express	= require('express');
-const mysql		= require("mysql2");
-const app		= express();
-const port		= 3000;
-const cors		= require('cors');
-const router	= require('./logica/router.js');
-var bodyParser	= require('body-parser');
+const express = require('express');
+const mysql = require("mysql2");
+const app = express();
+const port = 3000;
+const cors = require('cors');
+const router = require('./logica/router.js');
+var bodyParser = require('body-parser');
 require('dotenv').config();
 
 //Permite el acceso a la API desde el front
@@ -15,8 +15,8 @@ app.use(
 );
 
 //Permite el manejo de archivos grandes
-app.use(bodyParser.json({limit: '100mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 //Permite el manejo de JSON
 app.use(express.json());
@@ -26,15 +26,16 @@ app.listen(port, () => {
 	console.log(`Servicio corriendo en ${port}`);
 })
 
-//Carga de credenciales
+/*
+//Conexión a la base de datos
 const conexion = mysql.createConnection({
+	//Carga de credenciales
 	host: process.env.HOST,
 	user: process.env.USUARIO,
 	password: process.env.PASSWORD,
 	database: process.env.DATABASE
 });
 
-//Conexión a la base de datos
 conexion.connect((error) => {
 	if (error) {
 		console.error("No se pudo conectar a la BD: " + error);
@@ -43,8 +44,8 @@ conexion.connect((error) => {
 	console.log("Conectado a la DB");
 });
 
-//uso del router
-app.use('/', router);
-
 //Exporta la conexión
 module.exports = conexion;
+*/
+//uso del router
+app.use('/', router);
