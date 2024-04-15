e6f9e56d8aeb337d47ee61e3b6a5dfddc3d87361
leoleguizamon <anlleguizamongu@unal.edu.co>
2024-04-15 10:41:12 -0500

    min changes

--- Front/src/components/Elementos/FichaTercerosN.vue
+++ Front/src/components/Elementos/FichaTercerosN.vue
@@ -1,10 +1,18 @@
 <template>
-	<div class="bg-danger card">
+	<div class="bg-danger card p-1">
 		<div class="d-flex">
+			<div id="tipoTercero" class="card bg-black w-25 me-1">
+				<label for="idTercero" class="form-label">ID/NIT</label>
+				<div class="d-flex">
+					<input id="idTercero" class="form-control me-1" type="text" :value="ter.idTercero">
+					<input class="form-control" type="text" :value="ter.dv" style="width: 40px;">
+					
+				</div>
+			</div>
+			<div id="infoTercero" class="card bg-black w-75">
 
+			</div>
 		</div>
-
-
 	</div>
 </template>
 <script>
